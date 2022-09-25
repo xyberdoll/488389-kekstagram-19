59cb356b2305903946ece02f879cab0787775cab
xyberdoll <122668763+xyberdoll@users.noreply.github.com>
2022-09-25 09:17:58 +0000

    :space_invader: Added .gitattributes & .gitignore files

--- graph.old.c
+++ graph.old.c
@@ -74,4 +74,4 @@ int main(int argc, char const *argv[]) {
 		for(int j=i+1;j<k;j++) {
 			// printf("%d, %d\n", i+1, j+1);
 			for(int l=j+1;l<k;l++) {
-				// printf("
+				// printf("----> %d, %d, %d\n", i
