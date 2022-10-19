6124804f2857777688b7fffbf5b8d5d2727de8ba
xyberdoll <122668763+xyberdoll@users.noreply.github.com>
2022-10-19 15:23:34 +0000

    fixed bower.json

--- graph.old.c
+++ graph.old.c
@@ -94,4 +94,4 @@ int main(int argc, char const *argv[]) {
 		}
 	}
 	for(int i=0;i<q;i++) {
-		printf("============> %d, %d, %d\n", items[i][0].key, items[
+		printf("============> %d, %d, %d\n", items[i][0].key, items[i][0].value, item
