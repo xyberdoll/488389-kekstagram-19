a80b16b4990a2a142353d0153176d23f43d43e63
xyberdoll <122668763+xyberdoll@users.noreply.github.com>
2022-12-15 13:35:43 +0000

    try that again

--- graph.test.c
+++ graph.test.c
@@ -4,4 +4,5 @@
  * @Version : 1.0
  * @Programmer : Max
  * @Date : 2019-07-15
- * @Released under : https://github.com/BaseMax/RamseyNumber/blob/ma
+ * @Released under : https://github.com/BaseMax/RamseyNumber/blob/master/LICENSE
+ *
